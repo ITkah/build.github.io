d1065891a8587705d0d3626cd2a08d28640a0d47
ITkah <ilatkacenko132@gmail.com>
2021-10-05 14:55:03 +0300

    asd

--- js/common.js
+++ js/common.js
@@ -5,3 +5,29 @@ $(".menu").on("click", function(e){
 $(".close").on("click", function(e){
   $("nav").removeClass("nav_active");
 });
+
+var swiper = new Swiper(".swiper", {
+  loop: true,
+  centeredSlides: true,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    990: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  },
+  navigation: {
+    nextEl: ".object .right",
+    prevEl: ".object .left",
+  },
+});
